ef4ce226dc4cc0d3e3678deacb07b07d14af7e47
sahrulanwar01 <denanawar2001@gmail.com>
2024-08-22 15:44:36 +0700

    satu

--- next.config.js
+++ next.config.js
@@ -6,7 +6,9 @@ const nextConfig = {
             'localhost',
             'amazonaws.com',
             'tsuzumi-bucket.s3.ap-southeast-1.amazonaws.com',
-            'tsuzumi-bucket.s3.amazonaws.com'
+            'tsuzumi-bucket.s3.amazonaws.com',
+            'pelangiteknik.vercel.app',
+            'vercel.app'
         ],
     },
 };
