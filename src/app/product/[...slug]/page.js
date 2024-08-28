e6e9941877dbe651ac0485fa0d108fdff2116292
sahrulanwar01 <denanawar2001@gmail.com>
2024-08-28 14:48:39 +0700

    enam

--- src/app/product/[...slug]/page.js
+++ src/app/product/[...slug]/page.js
@@ -5,13 +5,17 @@ import { IdProduk, Produk } from "@/service/user";
 export const dynamic = 'force-dynamic'
 
 export default async function Page({ params }) {
-
+    BigInt.prototype.toJSON = function () {
+        return this.toString();
+    }
     const dataIdProduk = await IdProduk(params.slug)
     console.log('id', dataIdProduk);
 
     const dataProduk = await Produk(dataIdProduk)
     console.log('produk', dataProduk);
-
+    BigInt.prototype.toJSON = function () {
+        return this.toString();
+    }
     return (
         <HeaderFooter >
             <Product
